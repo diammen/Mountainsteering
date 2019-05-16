0ed288c0e251d49a6cff0b89d2cf3918c4a89e88
diammen <marcelnamdao@gmail.com>
2019-05-15 17:56:10 -0700

    add player movement and gun turret

---
+++ Assets/Scenes/GunTurret.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunTurret : MonoBehaviour {
+
+    public string xAxis, yAxis;
+    public float rotSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(Input.GetAxis(xAxis) * rotSpeed, 0, 0);
+    }
+}
---
+++ Assets/Scenes/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public string xAxis, yAxis;
+    public float movSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yRaw = Input.GetAxis(yAxis);
+        float xRaw = Input.GetAxis(xAxis);
+
+        transform.position = new Vector3(transform.position.x + xRaw * movSpeed * Time.deltaTime, transform.position.y, transform.position.z + yRaw * movSpeed * Time.deltaTime);
+    }
+}

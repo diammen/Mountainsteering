70578179342579d2e5ae06a70719681c235506ac
diammen <marcelnamdao@gmail.com>
2019-05-16 12:46:39 -0700

    add shooting

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -5,16 +5,50 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour {
 
     public GameObject[] bullets;
-    public float timeToFire;
-    public float timeStamp;
+    public Transform firePosition;
+    public string triggerAxis;
+    public float timeToFire, timeStamp;
+    public float bulletSpeed;
 
 	// Use this for initialization
 	void Start () {
-		
+        var bul = Resources.Load<GameObject>("bullet");
+
+        for (int i = 0; i < bullets.Length; i++)
+        {
+            var temp = Instantiate(bul, transform.position, transform.rotation);
+
+            bullets[i] = temp;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float triggerRaw = Input.GetAxis(triggerAxis);
+
+        if (triggerRaw != 0)
+        {
+            if (Time.time - timeStamp > timeToFire)
+            {
+                FireBullet();
+                timeStamp = Time.time;
+            }
+        }
 	}
+
+    void FireBullet()
+    {
+        foreach (GameObject bul in bullets)
+        {
+            if (!bul.activeSelf)
+            {
+                bul.SetActive(true);
+                bul.GetComponent<TrailRenderer>().Clear();
+                bul.transform.position = firePosition.position;
+                bul.transform.rotation = firePosition.rotation;
+                bul.GetComponent<Rigidbody>().AddForce(bul.transform.forward * bulletSpeed, ForceMode.Impulse);
+                break;
+            }
+        }
+    }
 }
